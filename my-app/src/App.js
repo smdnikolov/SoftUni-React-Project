3755c26c7b0c57410009b62f543792f57b7b29d4
smdnikolov <smdnikolov@gmail.com>
2020-08-10 09:23:30 +0300

    Scroll to top
    Added scroll-to-top component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Router from './router/Router'
 import Store from './Store'
 import Header from './components/header'
 import Footer from './components/footer'
+import ScrollToTop from './components/scroll-to-top'
 import { ToastContainer, Flip } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,6 +14,7 @@ const App = () => {
       <Store>
         <Header />
         <ToastContainer transition={Flip} autoClose={4000} hideProgressBar={true} closeOnClick />
+        <ScrollToTop />
         <Router />
         <Footer />
       </Store>
