b962f74203ffc76efa8c608dd1332c65099b2c29
smdnikolov <smdnikolov@gmail.com>
2020-08-11 12:31:28 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { key, authDomain, databaseURL, projectId, storageBucket, messagingSender
 import axios from 'axios'
 
 const firebaseConfig = {
-    apiKey: key,
-    authDomain: authDomain,
-    databaseURL: databaseURL,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_UR,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: "1:533289266506:web:1833febb1272c471fe25b3"
 };
 
 
