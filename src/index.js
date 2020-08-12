67ba9abf1e0255ebbba5eb2014ac0ad9156a8f82
smdnikolov <smdnikolov@gmail.com>
2020-08-12 18:24:34 +0300

    roll back css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../public/index.css';
+import './index.css';
 import App from './App';
 import Store from './Store'
 import { BrowserRouter } from 'react-router-dom'
