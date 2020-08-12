733030e23ee65a22264ceff41d7b3dfb6d66a023
smdnikolov <smdnikolov@gmail.com>
2020-08-12 08:45:07 +0300

    Fixed readme typos and added loader for imgs

--- src/components/browse-categories/index.js
+++ src/components/browse-categories/index.js
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 import SearchForm from '../../components/search-form'
 import categories from '../../utils/categories'
 
-const BrowseCategories = () => {
+const BrowseCategories = ({ setter, setting }) => {
+
+    const setLoading = (setter, setting) => {
+        setter(setting)
+    }
+
+
+
     const categoriesList = categories.map((category, index) =>
         <li key={index}>
             <Link to={'/category/' + category.link}>
-                <img src={category.url} alt="" width="100px" />
+                <img onLoad={setLoading(setter, setting)} src={category.url} alt="" width="100px" />
                 <p>{category.name}</p>
             </Link>
         </li>
--- src/route-componenets/home/index.js
+++ src/route-componenets/home/index.js
@@ -1,24 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, setState } from 'react'
+import Loader from '../../components/loader'
 import BrowseCategories from '../../components/browse-categories'
 
 
 
 const Home = () => {
 
+    const [loading, setLoading] = setState(true)
+
     useEffect(() => { localStorage.removeItem('prevPath') })
 
     return (
-        <div className="container search">
-            <div className="row">
-                <div className="col">
-                    <div className="jumbotron">
-                        <h1>Welcome to the Online Market</h1>
-                        <p>Browse the Categories to find what you need</p>
-                        <BrowseCategories />
-                    </div>
-                </div>
-            </div >
-        </div >
+        <div>
+            {loading
+                ? <div className="container jumbotron"><h1>Loading</h1><Loader /></div>
+                : <div className="container search">
+                    <div className="row">
+                        <div className="col">
+                            <div className="jumbotron">
+                                <h1>Welcome to the Online Market</h1>
+                                <p>Browse the Categories to find what you need</p>
+                                <BrowseCategories setter={setLoading} setting={false} />
+                            </div>
+                        </div>
+                    </div >
+                </div >
+            }
+        </div>
+
     )
 }
 
