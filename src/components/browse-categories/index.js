96319da7c1d79eb4468611265a7f9b2426f9e267
smdnikolov <smdnikolov@gmail.com>
2020-08-12 08:54:31 +0300

    reverted last

--- src/components/browse-categories/index.js
+++ src/components/browse-categories/index.js
@@ -3,18 +3,12 @@ import { Link } from 'react-router-dom'
 import SearchForm from '../../components/search-form'
 import categories from '../../utils/categories'
 
-const BrowseCategories = ({ setter }) => {
-
-    const setLoading = (setter) => {
-        setter(false)
-    }
-
-
+const BrowseCategories = () => {
 
     const categoriesList = categories.map((category, index) =>
         <li key={index}>
             <Link to={'/category/' + category.link}>
-                <img onLoad={setLoading(setter)} src={category.url} alt="" width="100px" />
+                <img src={category.url} alt="" width="100px" />
                 <p>{category.name}</p>
             </Link>
         </li>
--- src/route-componenets/home/index.js
+++ src/route-componenets/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, setState } from 'react'
+import React, { useEffect } from 'react'
 import Loader from '../../components/loader'
 import BrowseCategories from '../../components/browse-categories'
 
@@ -6,28 +6,20 @@ import BrowseCategories from '../../components/browse-categories'
 
 const Home = () => {
 
-    const [loading, setLoading] = setState(true)
-
     useEffect(() => { localStorage.removeItem('prevPath') })
 
     return (
-        <div>
-            {loading
-                ? <div className="container jumbotron"><h1>Loading</h1><Loader /></div>
-                : <div className="container search">
-                    <div className="row">
-                        <div className="col">
-                            <div className="jumbotron">
-                                <h1>Welcome to the Online Market</h1>
-                                <p>Browse the Categories to find what you need</p>
-                                <BrowseCategories setter={setLoading} />
-                            </div>
-                        </div>
-                    </div >
-                </div >
-            }
-        </div>
-
+        <div className="container search">
+            <div className="row">
+                <div className="col">
+                    <div className="jumbotron">
+                        <h1>Welcome to the Online Market</h1>
+                        <p>Browse the Categories to find what you need</p>
+                        <BrowseCategories />
+                    </div>
+                </div>
+            </div >
+        </div >
     )
 }
 
